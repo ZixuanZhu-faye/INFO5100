193b9420bd73b237c5fa80134200f691e9ce87f6
朱子萱 <zhuzixuan@zhuzixuandeMacBook-Pro.local>
2021-09-24 20:17:08 -0400

    upload project_5100

---
+++ Assignment1_Person/src/main/java/model/Person.java
@@ -0,0 +1,176 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+/**
+ *
+ * @author zhuzixuan
+ */
+public class Person {
+    private String Name;
+    private String geo_data;
+    private String birth;
+    private String Tel;
+    private String fax_num;
+    private String email;
+    private String secur_num;
+    private String medical_num;
+    private String health_num;
+    private String bank_num;
+    private String certi_num;
+    private String vehc_num;
+    private String device_num;
+    private String linkedln;
+    private String intnet_add;
+    private String bio_pic_add;
+    private String face_add;
+    private String other_num;
+
+    public String getName() {
+        return Name;
+    }
+
+    public void setName(String Name) {
+        this.Name = Name;
+    }
+
+    public String getGeo_data() {
+        return geo_data;
+    }
+
+    public void setGeo_data(String geo_data) {
+        this.geo_data = geo_data;
+    }
+
+    public String getBirth() {
+        return birth;
+    }
+
+    public void setBirth(String birth) {
+        this.birth = birth;
+    }
+
+    public String getTel() {
+        return Tel;
+    }
+
+    public void setTel(String Tel) {
+        this.Tel = Tel;
+    }
+
+    public String getFax_num() {
+        return fax_num;
+    }
+
+    public void setFax_num(String fax_num) {
+        this.fax_num = fax_num;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getSecur_num() {
+        return secur_num;
+    }
+
+    public void setSecur_num(String secur_num) {
+        this.secur_num = secur_num;
+    }
+
+    public String getMedical_num() {
+        return medical_num;
+    }
+
+    public void setMedical_num(String medical_num) {
+        this.medical_num = medical_num;
+    }
+
+    public String getHealth_num() {
+        return health_num;
+    }
+
+    public void setHealth_num(String health_num) {
+        this.health_num = health_num;
+    }
+
+    public String getBank_num() {
+        return bank_num;
+    }
+
+    public void setBank_num(String bank_num) {
+        this.bank_num = bank_num;
+    }
+
+    public String getCerti_num() {
+        return certi_num;
+    }
+
+    public void setCerti_num(String certi_num) {
+        this.certi_num = certi_num;
+    }
+
+    public String getVehc_num() {
+        return vehc_num;
+    }
+
+    public void setVehc_num(String vehc_num) {
+        this.vehc_num = vehc_num;
+    }
+
+    public String getDevice_num() {
+        return device_num;
+    }
+
+    public void setDevice_num(String device_num) {
+        this.device_num = device_num;
+    }
+
+    public String getLinkedln() {
+        return linkedln;
+    }
+
+    public void setLinkedln(String linkedln) {
+        this.linkedln = linkedln;
+    }
+
+    public String getIntnet_add() {
+        return intnet_add;
+    }
+
+    public void setIntnet_add(String intnet_add) {
+        this.intnet_add = intnet_add;
+    }
+
+    public String getBio_pic_add() {
+        return bio_pic_add;
+    }
+
+    public void setBio_pic_add(String bio_pic_add) {
+        this.bio_pic_add = bio_pic_add;
+    }
+
+    public String getFace_add() {
+        return face_add;
+    }
+
+    public void setFace_add(String face_add) {
+        this.face_add = face_add;
+    }
+
+    public String getOther_num() {
+        return other_num;
+    }
+
+    public void setOther_num(String other_num) {
+        this.other_num = other_num;
+    }
+    
+}
